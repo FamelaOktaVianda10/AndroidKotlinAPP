877462ec2b9267f40b8844a8481e8f6225b20afa
LOQ <jinemoch@gmail.com>
2024-10-08 10:50:02 +0700

    First Commit

---
+++ app/src/main/java/FamelaOktavianda/com/example/androidkotlinapp2/MainActivity.kt
@@ -0,0 +1,38 @@
+package FamelaOktavianda.com.example.androidkotlinapp2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+
+import FamelaOktavianda.com.example.androidkotlinapp2.databinding.ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvCounter.text = "1"
+
+    }
+    fun addNumber(v: View) {
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal + 1
+        binding.tvCounter.text = nextVal.toString()
+
+    }
+    fun resetNumber(v: View) {
+        binding.tvCounter.text = "0"
+    }
+
+    fun subtractNumber(v: View){
+        val currVal = binding.tvCounter.text.toString().toInt()
+        val nextVal = currVal - 1
+        binding.tvCounter.text = nextVal.toString()
+    }
+
+}
